8f09f0d4e620b5ec436a9c9583f2bc45580ba4eb
Yuxubinchen <2302370185@qq.com>
2021-06-19 20:47:13 +0800

    wu

--- c/聊天室.c/client.c
+++ c/聊天室.c/client.c
@@ -1,5 +1,54 @@
-#include"c_s.h"
+#include "c_s.h"
+char nums[50];
+void delu()
+{
+    printf("----------------------------\n");
+    printf("%5s","a.登陆\n");
+    printf("%5s","b.注册\n");
+    printf("%5s","q.退出\n");
+    printf("%5s","q.找回密码\n");
+    printf("----------------------------\n");
+}
+void sign()
+{   
+    char st1[20],st2[20];
+    printf("账号:");
+    fgets(stl1,20,stdin);
+    printf("密码:");
+    fgets(stl2,20,stdin);
+}
+void chuli()
+{
+    char t = fgetc(stdin);
+    switch (t)
+    {
+        case 'a':
+        
+        case "b":
+        case "q":
+
+    }
+}
 int main()
 {
-    
+    int sock_fd;
+    struct sockaddr_in cid, lid;
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0)
+    {
+        my_err("socket", __LINE__);
+    }
+    memset(&cid, 0, sizeof(cid));
+    cid.sin_port = port;
+    cid.sin_family = AF_INET;
+    printf("请输入服务器地址:");
+    fgets(nums, 50, stdin);
+    cu[strlen(nums) - 1] = '\0';
+    cid.sin_family = inet_aton(nums, &cid.sin_addr);
+    if (connect = (sock_fd, (struct sockaddr *)&cid), sizeof(cid) < 0)
+    {
+        my_err("connect", __LINE__);
+    }
+    delu();
+    chuli();
 }
--- c/聊天室.c/server.c
+++ c/聊天室.c/server.c
@@ -1,15 +1,22 @@
 #include "c_s.h"
 MYSQL mysql;
+char nums[50];
+void *body()
+{
+    int flag=recv(,nums,sizeof(nums),0);
+
+}
 void mysql_in_del(char buf)
 {
     int flag;
     flag = mysql_real_query(mysql, buf);
     if (flag)
     {
-        my_err(buf, __LINE__);
+        my_err("mysql_query", __LINE__);
     }
+    //注意调用以后，回返结果
 }
-void musql_select(char buf)
+void mysql_select(char buf)
 {
     int flag;
     MYSQL_RES *result;
@@ -18,12 +25,12 @@ void musql_select(char buf)
     flag = mysql_real_query(mysql, buf);
     if (flag)
     {
-        my_err(buf, __LINE__);
+        my_err("mysql_query", __LINE__);
     }
     result = mysql_store_result(&mysql);
     while (field = mysql_fetch_field(result))
     {
-        printf("-20s",field->name);
+        printf("-20s", field->name);
     }
     printf("\n");
     while (row = mysql_fetch_row(result))
@@ -38,17 +45,26 @@ void musql_select(char buf)
         printf("\n");
     }
 }
-void mysql_con()   //数据库初始化,连接
+void mysql_con() //数据库初始化,连接
 {
     unsigned int nums;
     MYSQL_RES *res = NULL;
     mysql_init(&mysql);
     mysql_library_init(0, NULL, NULL);
-    if (mysql_real_connect(&mysql, "127.0.0.1", "root", password, "chat", 0, NULL, 0) == NULL)
+    if (mysql_real_connect(&mysql, "127.0.0.1", "root", passwords, "chat", 0, NULL, 0) == NULL)
     {
-        my_err(mysql_real_connect, __LINE__);
+        my_err("mysql_real_connect", __LINE__);
     }
     mysql_set_character_set(&mysql, "utf8");
+}
+void registered()
+{
+}
+void sign()
+{
+}
+void send_file()
+{
 }
 int mysql_close()
 {
@@ -59,6 +75,79 @@ int mysql_close()
 }
 int main()
 {
-   
+    int lid, cid;
+    int ep_fd;
+    struct sockaddr_in client_addr, server_addr;
+    lid = socket(AF_INET, SOCK_STREAM, 0);
+    if (lid < 0)
+    {
+        my_err("socket", __LINE__);
+    }
     mysql_con();
-}
+    if (setsockopt(lid, SOL_SOCKET, SO_REUSEADDR, (void *)&opt_val, sizeof(int)) < 0)
+    {
+        my_err("setsockopt", __LINE__);
+    }
+    memset(server_addr, 0, sizeof(server_addr));
+    server_addr.sin_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (bind(lid, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+    {
+        my_err("bind", __LINE__);
+    }
+    if (listen(lid, 20) < 0)
+    {
+        my_err("listen", __LINE__);
+    }
+    ep_fd = epoll_create(n);
+    struct epoll_event ep_id;
+    struct epoll_event ep_ids[12];
+    ep_id.data.fd = lid;
+    ep_id.events = EPOLLIN;
+    epoll_ctl(ep_fd, EPOLL_CTL_ADD, lid, &ep_id);
+    int events = n + 2;
+    int pthread_id;
+    int tsize;
+    while (1)
+    {
+        epoll_wait(ep_fd, ep_ids, events, -1);
+        for (int i = 0; i < events; i++)
+        {
+            if (ep_ids[i].data.fd == id)
+            {
+                if (cid = accept(lid, &client_addr, sizeof(client_addr)) < 0)
+                {
+                    my_err("accept", __LINE__);
+                }
+                ep_id.data.fd = cid;
+                ep_id.events = EPOLLIN;
+                epoll_ctl(ep_fd, EPOLL_CTL_MOD, cid, &ep_fd);
+            }
+            else if (ep_ids[i].events & EPOLLIN)
+            {
+                if (tsize = recv(ep_ids[i].data.fd, nums, sizeof(nums), 0) < 0)
+                {
+                    my_err("recv", __LINE__);
+                }
+                else if (tsize == 0) //对端客户端关闭
+                {
+                    close(ep_ids[i].data.fd);
+                }
+                else
+                {
+                    if (pthread_create(&pthread_id, NULL, body, NULL))
+                    {
+                        my_err("thread_create", __LINE__);
+                    }
+                    pthread_detach(pthread_id);
+                    ep_ids[i].events = EPOLLOUT;
+                }
+            }
+            else if (ep_ids[i].events & EPOLLOUT)
+            {
+            }
+        }
+        mysql_close();
+        return 0;
+    }
