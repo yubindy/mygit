cd0276c970017291b18be67afe7bacc854acd623
Yuxubinchen <2302370185@qq.com>
2021-07-28 20:50:03 +0800

    wu

--- c/聊天室.c/chatroom.h
+++ c/聊天室.c/chatroom.h
@@ -7,9 +7,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-#include<signal.h>
+#include <signal.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
 #include <termios.h>
+#include <fcntl.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <sys/select.h>
@@ -17,17 +19,19 @@
 #include <sys/epoll.h>
 #include <pthread.h>
 #include "cJSON.h"
+#include <sys/sendfile.h>
+#include<time.h>
 #include <mysql/mysql.h>
 #define passwords "zhaozeyu1234"
 #define port 8573   //绑定端口
-#define size 50    ///可允许同时连接的最大客户端数量
+#define size 50     ///可允许同时连接的最大客户端数量
 typedef struct pack //定义包类型
 {
     char recv_name[10]; //名字
     char send_name[10];
     int send_id;
     int id;
-    int status;    
+    int status;
     int send_nums; //账号
     int recv_nums;
     char cho; //选项选择功能
@@ -35,32 +39,32 @@ typedef struct pack //定义包类型
     char nums[20];
 
 } pack;
-typedef struct node////定义链表存储上线用户,频繁修改数据也用于上线用户群聊交流
-{ 
-    char t[20];  //名字
+typedef struct node ////定义链表存储上线用户,频繁修改数据也用于上线用户群聊交流
+{
+    char t[20]; //名字
     int id;
     int groupid;
     struct node *next;
 } node;
-typedef struct pthnode  //保存信息的链表
+typedef struct pthnode //保存信息的链表
 {
-    char work[50];   //存储历史或好友名字的字符串
-    int status;  //好友状态
+    char work[50]; //存储历史或好友名字的字符串
+    int status;    //好友状态
     char name[20];
-    struct pthnode* next;
-}pthnode;
-typedef struct infonode  //私聊中收集信息的链表
+    struct pthnode *next;
+} pthnode;
+typedef struct infonode //私聊中收集信息的链表
 {
-     char name[10];
-     int send_id;
-     struct infonode *next;
-}infonode;
+    char name[10];
+    int send_id;
+    struct infonode *next;
+} infonode;
 typedef struct groupnode
 {
-    char name[10]; 
-    int id;       //群id
+    char name[10];
+    int id; //群id
     struct groupnode *next;
-}groupnode;
+} groupnode;
 
 extern MYSQL mysql;
 const char right[20] = "yes you are right";
@@ -85,5 +89,16 @@ void mima(char *s) //封装无回显示函数
     printf("请输入密码(20个字符以内):");
     scanf("%s", s);
     tcsetattr(0, TCSANOW, &old_ting);
+}
+void rand_file(char *s)   //生成随机目录
+{  int a,b;
+   srand((unsigned)time(NULL));
+   a=rand()%12+3;    
+   for(int i=0;i<a;i++)
+   {
+      b=rand()%26;
+      s[i]='a'+b;
+   }
+   return;
 }
 #endif
