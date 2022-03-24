ff1a93cc5b250f26c070d6fdf0d24a018b16d6e6
yuxubinchen <2302370185@qq.com>
2022-03-24 21:28:32 +0800

    update

--- c/1.c
+++ c/1.c
@@ -1,8 +1,39 @@
-#include<stdio.h>
-int main()
-{
-    int t=-1.23;
-    int s=t;
-    printf("%d",t/1);
-    return 0;
+#include <stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target){
+    int left = 0, right = numsSize-1,middle;
+    while(right>=left)
+    {
+        middle = (left+right)/2;
+        if(nums[middle]==target){
+            return middle;
+        }else if(nums[middle]>target){
+            right = middle-1;
+        }else{
+            left = left+1;
+        }
+    }
+    return left;
+}
+void insert(int *nums, int numsSize, int index, int target) {
+  int x=target,y;
+  for (int i = index; i <=numsSize; i++) {
+    y = nums[i];
+    nums[i] =x;
+    x=y;
+  }
+}
+int main() {
+  int num[20] = {1, 3, 5, 7, 8, 25, 37, 39, 40};
+  int s, numsize = 9;
+   printf("要输入的");
+  scanf("%d",&s);
+  int index = searchInsert(num, numsize, s);
+  printf("%d",index);
+  insert(num, numsize, index,s);
+  printf("\n");
+  for (int i = 0; i <= numsize; i++) {
+    printf("%d ", num[i]);
+  }
+  return 0;
 }
---
+++ c/suibian/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <sys/mman.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
