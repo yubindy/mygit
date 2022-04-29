50c4db05e019fb1b75a9854b4fdfc13f4710c34e
yuxubinchen <2302370185@qq.com>
2022-04-29 21:09:53 +0800

    update

--- leection/03.cpp
+++ leection/03.cpp
@@ -1,32 +1,3 @@
-// #include<stdio.h>
-// #include<string.h>
-// char s[]="abcabcbb";
-// int lengthOfLongestSubstring(char * s){
-//   int right=0,left=0;
-//   int a[3]={};
-//   int len=1;
-//   int t=strlen(s)-1;
-//   while(right<=t)
-//   {
-//       a[s[right]-'a']++;
-//       for(int i=0;i<3;i++)
-//       {
-//           while(a[i]>1)
-//           {
-//             len=len<right-left?right-left:len;
-//             a[s[right]-'a']--;
-//             left++;
-//           }
-//       }
-//     right++;
-//   }
-//   return len;
-// }
-// int main()
-// {
-//   printf("%d",lengthOfLongestSubstring(s));
-//   return 0;
-// }
 #include <map>
 #include <stdio.h>
 #include <string>
