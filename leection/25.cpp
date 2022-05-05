0cff5ac7ce8fb5d1418f046e5bb289a29ccaf5bd
yuxubinchen <2302370185@qq.com>
2022-05-05 23:07:59 +0800

    update

---
+++ leection/25.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+struct ListNode {
+     int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode *right,*left;
+    right=head;
+    left=head;
+    
+    }
+};
