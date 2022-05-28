0078e28553f47108c8e90cb3e4a6660fbd324673
yuxubinchen <2302370185@qq.com>
2022-05-28 21:42:13 +0800

    update

--- c++/suibian/gtest.cpp
+++ c++/suibian/gtest.cpp
@@ -5,7 +5,11 @@ int add(int a, int b) {
 TEST(testCase, test0) {
     EXPECT_EQ(add(2, 3), 5);
 }
+TEST(testCase, test0) {
+    EXPECT_EQ(add(7, 9), 16);
+}
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+    
 }
