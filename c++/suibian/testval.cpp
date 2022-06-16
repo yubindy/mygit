de363a240a9338571b20fe5fdf374d14f89b4eab
yuxubinchen <2302370185@qq.com>
2022-06-16 22:21:27 +0800

    update

---
+++ c++/suibian/testval.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <memory>
+#include <thread>
+#include <utility>
+class Test {
+    int a;
+    int b;
+
+    Test() = default;
+
+public:
+    template <class... Args>
+    std::shared_ptr<Test> create(Args &&...args) {
+        class EnableMakeShared : public Test {
+            EnableMakeShared(Args &&...arg) :
+                Test(std::forward<Args>(arg)...) {
+            }
+        };
+        return std::make_shared<EnableMakeShared>(std::forward<Args>(args)...);
+    }
+    Test(Test &&) {
+        printf("Move\n");
+    }
+};
+void ThreadMain(std::shared_ptr<Test> x) {
+    //
+    printf("Nothing");
+}
+int main() {
+    auto t = std::make_shared<Test>();
+    ThreadMain(std::move(t));
+    //auto t1 = std::thread(ThreadMain,t);
+    //t1.detach();
+}
