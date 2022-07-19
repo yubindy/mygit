7db9b0c2e6d8d2f762a427aa5d97acb4f076bab2
yuxubinchen <2302370185@qq.com>
2022-07-19 22:31:24 +0800

    update

--- leection/146.cpp
+++ leection/146.cpp
@@ -1,26 +1,40 @@
-#include<list>
-#include<unordered_map>
-class lrunode{
-public:
-    lrunode(int cap):n(cap){}
-   std::list<int> t;
-   std::unordered_map<int,int> s;
-   int n;
-};
+#include <bits/stdc++.h>
+using namespace std;
+
 class LRUCache {
 public:
-    LRUCache(int capacity) {
-        lrunode(capacity);
-
+    LRUCache(int capacity) :
+        ListSize(capacity),p(),s() {
     }
-    
+
     int get(int key) {
-
+        if (s.find(key) == s.end()) {
+            return -1;
+        }
+        auto t = s[key];
+        p.erase(t);
+        p.emplace_front(make_pair(t->first,t->second));
+        return s[key]->second;
     }
-    
+
     void put(int key, int value) {
-
+        if (s.find(key)!=s.end()){
+           s[key]->second=value;
+           p.splice(p.begin(),p,s[key]);
+           return;
+        }
+        p.emplace_front(make_pair(key, value));
+        s[key] = p.begin();
+        if (p.size() > ListSize) {
+            s.erase(p.back().first);
+            p.pop_back();
+        }
     }
+
+private:
+    int ListSize;
+    list<pair<int, int>> p;
+    map<int, list<pair<int, int>>::iterator> s;
 };
 
 /**
@@ -29,3 +43,17 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+int main() {
+    LRUCache *lRUCache = new LRUCache(2);
+    int t;
+    lRUCache->put(1, 1); // 缓存是 {1=1}
+    lRUCache->put(2, 2); // 缓存是 {1=1, 2=2}
+    t=lRUCache->get(1);    // 返回 1
+    lRUCache->put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+    t=lRUCache->get(2);    // 返回 -1 (未找到)
+    lRUCache->put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+    t=lRUCache->get(1);    // 返回 -1 (未找到)
+    t=lRUCache->get(3);    // 返回 3
+    t=lRUCache->get(4);    // 返回 4
+    return 0;
+}
