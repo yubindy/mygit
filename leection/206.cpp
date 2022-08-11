edb1c7ca661208acafa5554c83d39f754e066deb
yuxubinchen <2302370185@qq.com>
2022-08-11 20:57:51 +0800

    update

--- leection/206.cpp
+++ leection/206.cpp
@@ -1,17 +1,50 @@
-#include<bits/stdc++.h>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <bits/stdc++.h>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() :
+         val(0), next(nullptr) {
+     }
+     ListNode(int x) :
+         val(x), next(nullptr) {
+     }
+     ListNode(int x, ListNode *next) :
+         val(x), next(next) {
+     }
+ };
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head->)
+        if(!head||!head->next){
+            return head;
+        }
+        ListNode *a=head;
+        ListNode *b=nullptr;
+        ListNode *c=head->next;
+        while (a) {
+            c=a->next;
+            a->next=b;
+            b=a;
+            a=c;
+        }
+        return b;
     }
 };
+int main(){
+    Solution t;
+    ListNode *tt;
+    ListNode *st;
+    ListNode *rt;
+    st=new ListNode;
+    rt=st;
+    for(int i=0;i<5;i++){
+        tt=new ListNode;
+        tt->val=i+1;
+        rt->next=tt;
+        rt=rt->next;
+    }
+    t.reverseList(st->next);
+    return 0;
+}
