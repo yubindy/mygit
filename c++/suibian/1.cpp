be8fdd5ed5124bea2c9eeae47758effeda3d3fda
yuxubinchen <2302370185@qq.com>
2022-08-11 16:38:45 +0800

    update

--- c++/suibian/1.cpp
+++ c++/suibian/1.cpp
@@ -1,38 +1,39 @@
-#include <iostream>
-#include <future>
-#include <memory>
-#include <thread>
-#include <memory.h>
-
-int fun(int x, std::promise<int>& p) {
-	x++;
-	x *= 10;
-	p.set_value(x);
-	std::cout << std::this_thread::get_id() << std::endl;
-	return x;
-}
-class A{
+#include <bits/stdc++.h>
+#include <ratio>
+using namespace std;
+int main(){
+	std::queue<int> s;
+	std::deque<int> s1;
+}class MinStack {
 public:
-	A();
-	virtual ~A()=0;
-	virtual int mat(int t)=0;
-
-};
-class B : public A{
-public:
-	B();
-	virtual ~B()=0;
+    /** initialize your data structure here. */
+    MinStack() {
+        index=0;
+    }
+    
+    void push(int x) {
+		int mins;
+		if(index==0){
+			mins=x;
+		}
+        mins=mins>x?x:mins;
+        fnt.emplace_back(make_pair(x,mins));
+        index++;
+    }
+    
+    void pop() {
+		fnt.pop_back();
+		index--;
+    }
+    
+    int top() {
+        return fnt[index-1].first;
+    }
+    
+    int min() {
+		return fnt[index-1].second;
+    }
 private:
-	std::shared_ptr<>
+    vector<pair<int,int>> fnt;
+    int index;
 };
-
-int main()
-{
-	std::promise<int> p;
-	std::future<int> fu = p.get_future();        // 并将结果返回给future
-	std::thread t(fun, 1, std::ref(p));
-	std::cout << fu.get() << std::endl;          // 当promise还没有值的时候在此等待
-	std::cout << std::this_thread::get_id() << std::endl;
-	t.join();
-	return 0;
-}
