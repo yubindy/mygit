36189da6a3c68bc416b7d3f3112cbb84417a7757
yuxubinchen <2302370185@qq.com>
2022-10-25 17:23:37 +0800

    update

---
+++ c++/suibian/teststirng_view.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main() {
+    std::string cstr("yangxunwu");
+    std::string_view stringView1(cstr.data());
+    std::string_view stringView2(cstr.data(), 4);
+    std::cout << "stringView1: " << stringView1 << ", stringView2: " << stringView2 << std::endl;
+    std::cout << "stringView1: " << stringView1.size() << ", stringView2: " << stringView2.data() << std::endl;
+}
