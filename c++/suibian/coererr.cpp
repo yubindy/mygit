832b4b12584e25f201a5ae0e1f2d0f3fe746be1a
yuxubinchen <2302370185@qq.com>
2022-10-27 23:19:56 +0800

    update

---
+++ c++/suibian/coererr.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+void func(int *p) {
+    printf("%d", *p);
+}
+int main() {
+    int *p = nullptr;
+    func(p);
+    return 0;
+}
---
+++ leection/54.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        if(n==0&&m==0)
+        int all=m*j;
+        vector<int> p;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(p.emplace_back(matrix[i][j]));
+                aall++;
+            }
+        }
+        return matrix;
+
+    }
+};
+int main() {
+    return 0;
+}
