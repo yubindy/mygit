a321f67fdf56b0b8612360d813f5557a738dc219
yuxubinchen <2302370185@qq.com>
2022-12-26 14:29:27 +0800

    update

--- leection/34t.cpp
+++ leection/34t.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int right,left;
+        int right=nums.size()-1,left=0;
         vector<int> rul;
         bool flag=false;
         while(left<right){
@@ -15,16 +15,31 @@ public:
                 left=mid;
                 right=mid;
                 flag=true;
+                break;
+            }
+            if(left+1==right){
+                if(nums[left]==target||nums[right]==target){
+                   flag=true;
+                   if(nums[left]==target){
+                       right=left;
+                   }else{
+                       left=right;
+                   }
+                }
+                break;
             }
         }
-        if (flag){
+        if(nums.size()==1&&target==nums[0]){
+            flag=true;
+        }
+        if (!flag){
             rul.emplace_back(-1);
             rul.emplace_back(-1);
         }else{
-            while(left>=0&&nums[left]==target){
+            while(left>0&&nums[left-1]==target){
                 left--;
             }
-            while(right<nums.size()&&nums[right]==right){
+            while(right<nums.size()-1&&nums[right+1]==target){
                 right++;
             }
             rul.emplace_back(left);
@@ -34,5 +49,9 @@ public:
     }
 };
 int main(){
-
-}
+    Solution t;
+    vector<int> n{5,7,7,8,8,10};
+    auto s=t.searchRange(n, 6);
+    printf("left: %d right: %d",s[0],s[1]);
+    return 0;
+}   
