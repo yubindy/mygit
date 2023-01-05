c59a667d56746208de818f52244d01835f5cf61d
yuxubinchen <2302370185@qq.com>
2023-01-05 12:22:33 +0800

    update

--- leection/78.cpp
+++ leection/78.cpp
@@ -1,10 +1,23 @@
 #include "leection.h"
 class Solution {
 public:
+    vector<int> p;
+    vector<vector<int>> rul;
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+        dfs(0,nums);
+        return rul;
+    }
+    void dfs(int index,vector<int>&nums){
+        if(index==nums.size()){
+            rul.emplace_back(p);
+            return;
+        }
+        p.emplace_back(nums[index]);
+        dfs(index+1,nums);
+        p.pop_back();
+        dfs(index+1,nums);
     }
 };
 int main(){
-    
+
 }
