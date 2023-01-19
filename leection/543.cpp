ebf2d0aff67438ab921d1c5d1d879bb9f7610125
yuxubinchen <2302370185@qq.com>
2023-01-20 00:07:26 +0800

    update

--- leection/543.cpp
+++ leection/543.cpp
@@ -1,14 +1,21 @@
 #include "leection.h"
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(!root){
-        return 0;
+    int val;
+    int dfs(TreeNode* root) {
+        if(root==nullptr){
+            return 0;
         }
         int l=0,r=0;
-        l=diameterOfBinaryTree(root->left);
-        r=diameterOfBinaryTree(root->right);
+        l=dfs(root->left);
+        r=dfs(root->right);
+        val=max(l+r+1,val);
         return l>r?l+1:r+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        val=1;
+        dfs(root);
+        return val-1;
     }
 };
 int main(){
