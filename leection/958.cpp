934a37988e0dd307830d3773d67b188183020ddd
yuxubinchen <2302370185@qq.com>
2023-01-27 23:59:33 +0800

    update

---
+++ leection/958.cpp
@@ -0,0 +1,11 @@
+
+#include"leection.h"
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+
+    }
+};
+int main(){
+    
+}
--- leection/offer26.cpp
+++ leection/offer26.cpp
@@ -2,49 +2,7 @@
 #include <cstddef>
 class Solution {
 public:
-    bool rul;
     bool isSubStructure(TreeNode* A, TreeNode* B) {
-        if(!A){
-            return false;
-        }
-        bool p=false;;
-        if(A->val==B->val){
-            p=issub(A,B);
-            if(p==true){
-                return p;
-            }
-        }
-        if(A->left){
-            isSubStructure(A->left,B->left);
-        }
-        if(A->right){
-            isSubStructure(B->right,B->right);
-        }
-        return p;
-    }
-    bool issub(TreeNode* a, TreeNode* b){
-        if(a==nullptr||b==nullptr&&a!=b){
-            return false;
-        }
-        if(a->val!=b->val){
-            return false;
-        }else{
-            return true;
-        }
-        bool rul;
-        if(a->left||b->left){
-            rul=issub(a->left,b->left);
-        }
-        if(rul==false){
-            return rul;
-        }
-        if(a->right||b->right){
-            rul=issub(a->right,b->right);
-        }
-        return rul;
-
+        
     }
 };
-int main(){
-
-}
