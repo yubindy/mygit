5888ef628a04d41dee62e7912530297b2f5b5ceb
yuxubinchen <2302370185@qq.com>
2023-01-31 00:10:03 +0800

    update

--- leection/55.cpp
+++ leection/55.cpp
@@ -2,10 +2,13 @@
 class Solution {
 public:
     bool canJump(vector<int> &nums) {
+        if(nums[0]==0&&nums.size()==1){
+            return true;
+        }
         int n = nums.size() - 1;
         int p = 0;
-        for (int i = n; i > 0; i--) {
-            if (nums[i] == p) {
+        for (int i = n-1; i >= 0; i--) {
+            if (nums[i] <= p) {
                 nums[i]=0;
                 p++;
             } else {
@@ -16,5 +19,8 @@ public:
     }
 };
 int main(){
-    
+    vector<int> p{1,0,1,0};
+    Solution t;
+    printf("%d",t.canJump(p));
+    return 0;
 }
