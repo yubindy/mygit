3a8f58e416d3899497499e3de62678dff95cf4f9
yuxubinchen <2302370185@qq.com>
2023-02-01 17:13:59 +0800

    update

--- leection/offer26.cpp
+++ leection/offer26.cpp
@@ -4,42 +4,45 @@ class Solution {
 public:
     bool rul;
     bool isSubStructure(TreeNode* A, TreeNode* B) {
+        return isture(A,B);
+    }
+    bool isture(TreeNode* A, TreeNode* B) {
         if(A==nullptr||B==nullptr&&A!=B){
-            return false;
-        }
-        if(!A){
             return false;
         }
         bool p=false;;
         if(A->val==B->val){
+            printf("panduan %d",A->val);
             p=issub(A,B);
             if(p==true){
                 return p;
+            }else{
+                printf("rul %d",p);
             }
         }
-        if(A->left){
-            isSubStructure(A->left,B->left);
+        p=isture(A->left,B);
+        if(p==true){
+                return p;
         }
-        if(A->right){
-            isSubStructure(B->right,B->right);
-        }
+        p=isture(A->right,B);
         return p;
     }
     bool issub(TreeNode* a, TreeNode* b){
-        if(a==nullptr||b==nullptr&&a!=b){
+        if(a==nullptr&&b!=nullptr){
             return false;
-        }
-        if(a->val!=b->val){
-            return false;
-        }else{
+        }else if(a!=nullptr&&b==nullptr||a==nullptr&&b==nullptr){
             return true;
         }
-        bool rul;
+        printf(" %d ",a->val);
+        if(a->val!=b->val){
+            return false;
+        }
+        bool rul=true;
         if(a->left||b->left){
             rul=issub(a->left,b->left);
-        }
-        if(rul==false){
+            if(rul==false) {
             return rul;
+            }
         }
         if(a->right||b->right){
             rul=issub(a->right,b->right);
