c0fbc8cce91daaf3cb8a7328123bdef57114ef77
yuxubinchen <2302370185@qq.com>
2023-02-12 23:56:48 +0800

    update

---
+++ leection/offer095.cpp
@@ -0,0 +1,13 @@
+#include "leection.h"
+#include <vector>
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.size(),n2=text2.size();
+        vector<vector<int>> p(n1+1,vector<int>(n2+1));
+        
+    }
+};
+int main(){
+
+}
